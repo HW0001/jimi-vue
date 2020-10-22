0552a1b1079097d643637e8ec0944be90355d713
HW0001 <1600860624@qq.com>
2020-10-22 23:00:42 +0800

    完成icon组件以及nav样式

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,5 @@ export default {
 }
 </script>
 <style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/global.css";
 </style>
--- src/components/MenuNav.vue
+++ src/components/MenuNav.vue
@@ -1,25 +1,42 @@
 <template>
-<div>
-  <router-link to="/home">
-    <svg class="icon"><use xlink:href="#icon-label"></use></svg>
-    账本</router-link>|
-  <router-link to="/labels">标签</router-link>|
-  <router-link to="/statistics">统计</router-link>
-</div>
+  <nav>
+      <router-link to="/labels">
+      <icon name="icon-label" />
+      <span>标签</span></router-link>
+      <router-link to="/home">
+      <icon name="icon-home" />
+      <span>账本</span></router-link>
+      <router-link to="/statistics">
+      <icon name="icon-statistics" />
+        <span>统计</span></router-link>
+  </nav>
 </template>
 
 <script lang='ts'>
-import label from '@/assets/icons/label.svg'
-
-console.log(label);
+import Icon from "@/components/Icon.vue";
 export default {
-  name: "Menunav"
+  name: "MenuNav",
+  components: {Icon}
 }
 </script>
 
 <style lang='scss' scoped>
-.icon{
-  width: 1em;
-  height: 1em;
+nav{
+  display: flex;
+  box-shadow: 0  0 3px rgb(0,0,0,.3);
+  >a{
+    width: 33.33%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3px 0;
+    .icon{
+      width: 32px;
+      height: 32px;
+    }
+    span{
+      font-size: 12px;
+    }
+  }
 }
 </style>
