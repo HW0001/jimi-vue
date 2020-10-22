155cde2d113d69fe35fb9e86b950da1e8a15d125
HW0001 <1600860624@qq.com>
2020-10-22 22:20:42 +0800

    增加加icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang='ts'>
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+export default {
+  name: "Icon"
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
